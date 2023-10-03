6a309aa6bae14137b0b914729d770bb3502637b9
Aayush <aayushg2908@gmail.com>
2023-10-03 16:47:17 +0530

    Ecommerce Store

---
+++ app/(routes)/page.tsx
@@ -0,0 +1,23 @@
+import getBillboard from "@/actions/get-billboard";
+import getProducts from "@/actions/get-products";
+import Billboard from "@/components/ui/billboard";
+import Container from "@/components/ui/container";
+import ProductList from "@/components/product-list";
+
+const HomePage = async () => {
+  const products = await getProducts({ isFeatured: true });
+  const billboard = await getBillboard("0d6cfa42-f096-4c4d-b48d-44216e4701b8");
+
+  return (
+    <Container>
+      <div className="space-y-10 pb-10">
+        <Billboard data={billboard} />
+        <div className="flex flex-col gap-y-8 px-4 sm:px-6 lg:px-8">
+          <ProductList title="Featured Products" items={products} />
+        </div>
+      </div>
+    </Container>
+  );
+};
+
+export default HomePage;
